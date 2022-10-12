792cf83d4dbec3ad32a1e735d3f020ae70826738
Radoslav Gerganov <rgerganov@gmail.com>
2022-10-12 15:12:21 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "ggwave/ggwave.h"
+#include "pico/stdlib.h"
+#include "pico/pdm_microphone.h"
+#include "tusb.h"
+
+const int sampleRate = 16000;
+const int bufsize = 128;
+const int samplesPerFrame = 128;
+
+int16_t sampleBuffer[bufsize];
+volatile int samplesRead = 0;
+GGWave ggwave;
+GGWave::TxRxData result;
+
+// microphone configuration
+const struct pdm_microphone_config config = {
+    // GPIO pin for the PDM DAT signal
+    .gpio_data = 0,
+
+    // GPIO pin for the PDM CLK signal
+    .gpio_clk = 1,
+
+    // PIO instance to use
+    .pio = pio0,
+
+    // PIO State Machine instance to use
+    .pio_sm = 0,
+
+    // sample rate in Hz
+    .sample_rate = sampleRate,
+
+    // number of samples to buffer
+    .sample_buffer_size = bufsize,
+};
+
+void on_pdm_samples_ready()
+{
+    // callback from library when all the samples in the library
+    // internal sample buffer are ready for reading
+    samplesRead = pdm_microphone_read(sampleBuffer, bufsize);
+}
+
+int main(void)
+{
+    // initialize stdio and wait for USB CDC connect
+    stdio_init_all();
+    while (!tud_cdc_connected()) {
+        tight_loop_contents();
+    }
+
+    printf("Hello, this is ggtag!\n");
+    ggwave.setLogFile(nullptr);
+    auto p = GGWave::getDefaultParameters();
+    p.payloadLength   = 8;
+    p.sampleRateInp   = sampleRate;
+    p.sampleRateOut   = sampleRate;
+    p.sampleRate      = sampleRate;
+    p.samplesPerFrame = samplesPerFrame;
+    p.sampleFormatInp = GGWAVE_SAMPLE_FORMAT_I16;
+    p.sampleFormatOut = GGWAVE_SAMPLE_FORMAT_U8;
+    p.operatingMode   = GGWAVE_OPERATING_MODE_RX | GGWAVE_OPERATING_MODE_TX | GGWAVE_OPERATING_MODE_USE_DSS | GGWAVE_OPERATING_MODE_TX_ONLY_TONES;
+
+    GGWave::Protocols::tx().disableAll();
+    GGWave::Protocols::rx().disableAll();
+    GGWave::Protocols::rx().toggle(GGWAVE_PROTOCOL_MT_NORMAL, true);
+    GGWave::Protocols::rx().toggle(GGWAVE_PROTOCOL_MT_FAST, true);
+    GGWave::Protocols::rx().toggle(GGWAVE_PROTOCOL_MT_FASTEST, true);
+    GGWave::Protocols::rx().toggle(GGWAVE_PROTOCOL_DT_NORMAL, true);
+    GGWave::Protocols::rx().toggle(GGWAVE_PROTOCOL_DT_FAST, true);
+    GGWave::Protocols::rx().toggle(GGWAVE_PROTOCOL_DT_FASTEST, true);
+    // Print the memory required for the "ggwave" instance:
+    ggwave.prepare(p, false);
+    printf("Required memory by the ggwave instance: %d\n", ggwave.heapSize());
+    if (!ggwave.prepare(p, true)) {
+        printf("ggwave initialization failed\n");
+        while (1) { tight_loop_contents(); }
+    }
+    printf("ggwave initialized successfully\n");
+
+    // initialize the PDM microphone
+    if (pdm_microphone_init(&config) < 0) {
+        printf("PDM microphone initialization failed!\n");
+        while (1) { tight_loop_contents(); }
+    }
+
+    // set callback that is called when all the samples in the library
+    // internal sample buffer are ready for reading
+    pdm_microphone_set_samples_ready_handler(on_pdm_samples_ready);
+
+     // start capturing data from the PDM microphone
+    if (pdm_microphone_start() < 0) {
+        printf("PDM microphone start failed!\n");
+        while (1) { tight_loop_contents(); }
+    }
+    int niter = 0;
+    uint32_t totalSamples = 0;
+    while (1) {
+        // wait for new samples
+        while (samplesRead == 0) { tight_loop_contents(); }
+        // store and clear the samples read from the callback
+        int sampleCount = samplesRead;
+        samplesRead = 0;
+
+        auto tStart = to_ms_since_boot(get_absolute_time());
+        //printf("trying to decode...\n");
+        ggwave.decode(sampleBuffer, sampleCount*sizeof(int16_t));
+        auto tEnd = to_ms_since_boot(get_absolute_time());
+        if (++niter % 10 == 0) {
+            //printf("time: %d\n", tEnd - tStart);
+            // Check if decode() is taking too long
+            if (tEnd - tStart > 1000*(float(sampleCount)/sampleRate)) {
+                printf("Warning: decode() took too long to execute!\n");
+            }
+        }
+
+        // Check if we have successfully decoded any data:
+        int nr = ggwave.rxTakeData(result);
+        if (nr > 0) {
+            printf("Received data with length %d bytes\n", nr); // should be equal to p.payloadLength
+            printf("%s\n", result.data());
+        }
+    }
+    return 0;
+}
