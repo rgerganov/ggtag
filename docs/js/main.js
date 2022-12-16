ec6e179e82e32429bedd0e1795e4bd04c596f3fb
Radoslav Gerganov <rgerganov@gmail.com>
2022-12-16 16:40:48 +0200

    Use onRuntimeInitialized() instead of onLoad()

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function onLoad()
+function onRuntimeInitialized()
 {
     const params = new Proxy(new URLSearchParams(window.location.search), {
         get: (searchParams, prop) => searchParams.get(prop),
